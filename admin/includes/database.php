007e63e7449352773dd784058da5b270f3ccdefc
mirzahab85 <mirza.habul@telemach.ba>
2021-08-21 12:34:04 +0200

    Automatic DB Connection Setup

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -14,7 +14,7 @@ class Database {
 
     public function open_db_connetion(){
     
-        $this->connection = mysqli_connection(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
         if(mysqli_errno()){
             
@@ -25,6 +25,5 @@ class Database {
 }
 
 $database = new Database();
-$database->open_db_connetion();
 
 ?>
