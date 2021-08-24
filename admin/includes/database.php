16dc11ebb38117426717646c481156c18d4df994
mirzahab85 <mirza.habul@telemach.ba>
2021-08-24 11:37:10 +0200

    Improving our Connection to be more OOP

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -14,18 +14,22 @@ class Database {
 
     public function open_db_connetion(){
     
-        $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        //$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-        if(mysqli_errno()){
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+        if($this->connection->connection_errno){
             
-            die("Database connection failed badly" . mysqli_error());
+            die("Database connection failed badly" . $this->connection->connect_error);
             
         }       
     }
     
     public function query($sql) {
         
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
+
+        $this->confirm_query($result);
         
         return $result;
     
@@ -34,14 +38,20 @@ class Database {
     private function confirm_query($query){
 
         if(!$result) {
-        die("Quary Failed");
+            
+            die("Quary Failed" . $this->connection->error);
         }
     }
 
     public function escape_string($string){
         
-        $esaped_string = mysqli_real_escape_string($this->connection,$string);
+        $esaped_string = $this->connection->rel_escape_string($string);
         return $esaped_string;
+    }
+
+    public function the_insert_id(){
+        
+    return $this->connection->insert_id;
     }
 }
 
