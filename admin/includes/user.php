ab162f7ebbe28f98322216c69780568498a287b8
mirzahab85 <mirza.habul@telemach.ba>
2021-09-06 17:26:37 +0200

    Short Way Auto Instantiation

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -29,15 +29,24 @@ return $result_set;
     
 }
 
-public static function instantation($found_user) {
+public static function instantation($the_record) {
     
     $the_object = new self;
 
-    $the_object->id = $found_user['id'];
-    $the_object->username = $found_user['username'];
-    $the_object->password = $found_user['password'];
-    $the_object->first_name = $found_user['first_name'];
-    $the_object->last_name = $found_user['last_name'];
+    // $the_object->id = $found_user['id'];
+    // $the_object->username = $found_user['username'];
+    // $the_object->password = $found_user['password'];
+    // $the_object->first_name = $found_user['first_name'];
+    // $the_object->last_name = $found_user['last_name'];
+
+foreach($the_record as $the_attribute => $value) {
+
+if($the_object->has_the_attribute($the_attribute)) {
+
+    $the_object->the_attribute = $value;
+}
+        
+}
 
     return $the_object;
 
