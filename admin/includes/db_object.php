d8fed56b5610f7387360fadb63d7b563466984fe
mirzahab85 <mirza.habul@telemach.ba>
2022-01-19 23:44:27 +0100

    Fixing Double Return

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -1,18 +1,15 @@
 <?php
 
-class Db_oject {
-    
+class Db_oject 
+    {
     public static function find_all() {
-    return self::find_this_query("SELECT * FROM ". self::$db_table . " ");
-        
+    return self::find_this_query("SELECT * FROM ". self::$db_table . " ");   
     }
         
     public static function find_by_id($user_id) {
     global $database;
     $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id= $user_id LIMIT 1"); 
-    return !empty($the_result_array) ? array_shift($the_result_array) : falase;
-    return $found_user; 
-        
+    return !empty($the_result_array) ? array_shift($the_result_array) : false;   
     }
 
         public static function find_this_query($sql) {
@@ -20,13 +17,10 @@ class Db_oject {
     $result_set = $database->query($sql);
     $the_object_array = array();
     while($row = mysqli_fetch_array($result_set)) {
-    
-    $the_object_array[] = self::instantation($row);    
-            
+    $the_object_array[] = self::instantation($row);       
     }
     
-    return $the_object_array;
-        
+    return $the_object_array;   
     }
 
     public static function instantation($the_record) {
