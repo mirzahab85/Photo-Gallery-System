b6924ad07e27bacbac281097ff92a93d8904d590
mirzahab85 <mirza.habul@telemach.ba>
2022-01-30 16:50:02 +0100

    Save Method Part #1

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -4,7 +4,7 @@ class Photo extends Db_object {
 
     protected static $db_table = "photos";
         protected static $db_table_fields = array('photo_id', 'title', 'description', 'filename', 'type', 'size');
-        public $Photo_id;
+        public $photo_id;
         public $title;
         public $description;
         public $filename;
@@ -37,15 +37,38 @@ class Photo extends Db_object {
                 }elseif ($file['error'] !=0) {
                     $this->errors[] = $this->upload_errors_array[$file['error']];
                     return false;
-                }
+                } else {
                 
                 $this->filename = basename($file['name']);
                 $this->tmp_path = $file['tmp_name'];
                 $this->type     = $file['type'];
                 $this->size     = $file['size'];
-
         }
 
 } 
+
+public function save() {
+
+    if($this->Photo_id) {
+        $this->update();
+    } else {
+        
+        if(!empty($this->errors)) {
+        return false;
+        $this->create();
+    }
+
+    if(empty($this->filename) || empty($this->tmp_path)) {
+        $this->errors[] = "the file was not available";
+        return false;
+
+    $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->filename;
+    
+    }
+}
+
+}
+
+}
 
 ?>
