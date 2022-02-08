fbdd345b1cfd98f12a23bba080eb9942619c6e7f
mirzahab85 <mirza.habul@telemach.ba>
2022-02-08 21:04:24 +0100

    Setting up the right redirect paths for delete

--- admin/photos.php
+++ admin/photos.php
@@ -51,7 +51,7 @@ $photos = Photo::find_all();
 
                                     <div class="pictures_link">
 
-                                        <a href="delete_photo.php/?id=<?php echo $photo->id; ?>">Delete</a>
+                                        <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
                                         <a href="#">Edit</a>
                                         <a href="#">View</a>
 
